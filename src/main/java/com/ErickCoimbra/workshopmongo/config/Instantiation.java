aecc963d710688b709df7de8d4cc7cd37108e212
Erick de Oliveira <erick23coimbra@gmail.com>
2019-11-30 11:12:29 -0300

    Referenciando os posts do usuário

--- src/main/java/com/ErickCoimbra/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ErickCoimbra/workshopmongo/config/Instantiation.java
@@ -46,7 +46,8 @@ public class Instantiation implements CommandLineRunner {
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 		postReposiroty.saveAll(Arrays.asList(post1,post2));
 		
-		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userReposiroty.save(maria);
 	}
 
 }
